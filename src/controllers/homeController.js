28e650be87c1f90be49f3e9f25794e60b959af5d
Vu Tam R <vutam311291@gmail.com>
2023-09-19 22:30:54 +0700

    create navbar and ejs loop data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,17 +3,22 @@ import connection from '../configs/connectDB';
 let getHomepage = (req, res) => {
 	// logic
 
-	// let data = [];
+	let data = [];
 	// simple query
 	connection.query('SELECT * FROM `users`', function (err, results, fields) {
-		console.log('---check mysql: ');
 		let rows = results.map((row) => {
-			return row;
+			data.push({
+				id: row.id,
+				firstname: row.firstname,
+				lastname: row.lastname,
+				email: row.email,
+				address: row.address,
+			});
 		});
 		// console.log(results); // results contains rows returned by server
-		// console.log(rows);
+		console.log(data);
 
-		return res.render('test/index.ejs', { data: JSON.stringify(rows) });
+		return res.render('index.ejs', { dataUser: data, test: 'test...' });
 	});
 };
 
